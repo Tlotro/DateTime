0421508eeb49b27c0d49003ed52ba4aaf98e7648
Pletnev Michail <pletnev@sfedu.ru>
2021-10-21 09:40:13 +0300

    Update DateTime file

--- DateTime.pas
+++ DateTime.pas
@@ -4,19 +4,25 @@ begin
   result := ((Time.Year div 4 = 0) and (Time.Year div 100 <> 0)) or (Time.Year div 400 = 0)
 end;
 
-function LaterInYear(Day1,Day2: DateTime): DateTime;
+function LaterInYear(Day1, Day2: DateTime): DateTime;
 begin
   if (Day1.Month > Day2.Month) or ((Day1.Month = Day2.Month) and (Day1.Day > Day2.Day)) then
-    result := Day1;
+    result := Day1
   else 
     result := Day2;
 end;
 
-function DaysInYear(Year: integer):integer;
+function DaysInYear(Year: integer): integer;
 begin
   result := 365;
   if IsLeapYear(Year) then
     result += 1;
+end;
+
+function DaysInYearRange(Year1, Year2: integer): integer;
+begin
+  for var i := Year1 to Year2 do
+    result += DaysInYear(i);
 end;
 
 begin
