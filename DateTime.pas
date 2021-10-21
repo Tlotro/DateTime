3b7967eafbe86f51ad1a363ed0ec6e7f120a4d9d
Pletnev Michail <pletnev@sfedu.ru>
2021-10-21 09:14:54 +0300

    Change DateTime file

--- DateTime.pas
+++ DateTime.pas
@@ -4,6 +4,10 @@ begin
   result := ((Time.Year div 4 = 0) and (Time.Year div 100 <> 0)) or (Time.Year div 400 = 0)
 end;
 
+function SecondsInMinute : integer;
+begin
+  result := 60;
+end;
 
 begin
 end. 
