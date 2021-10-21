dda2741e9ca148ca1ee9c6f7f65f0e82e462066e
Pletnev Michail <pletnev@sfedu.ru>
2021-10-21 09:32:41 +0300

    Update DateTime file

--- DateTime.pas
+++ DateTime.pas
@@ -4,6 +4,13 @@ begin
   result := ((Time.Year div 4 = 0) and (Time.Year div 100 <> 0)) or (Time.Year div 400 = 0)
 end;
 
+function LaterInYear(Day1,Day2: DateTime): DateTime;
+begin
+  if (Day1.Month > Day2.Month) or ((Day1.Month = Day2.Month) and (Day1.Day > Day2.Day)) then
+    result := Day1;
+  else 
+    result := Day2;
+end;
 
 begin
 end. 
